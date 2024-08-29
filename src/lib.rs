e4cdd2ace301ec1b84b4d4c6b0daadf16b8ec1b8
IvoryDuke <ivory.duke@skiff.com>
2024-08-29 16:00:41 +0200

    0.6.6

--- src/lib.rs
+++ src/lib.rs
@@ -56,75 +56,6 @@ pub use crate::{
 };
 
 //=======================================================================//
-// MACROS
-//
-//=======================================================================//
-
-/// Loads the desired [`Thing`]s as an available resource coded into the executable.
-/// # Example
-/// ```
-/// use hill_vacuum::{hardcoded_things, MapThing, Thing};
-///
-/// struct Test;
-///
-/// impl MapThing for Test
-/// {
-///     fn thing() -> Thing { Thing::new("test", 0, 32f32, 32f32, "test").unwrap() }
-/// }
-///
-/// let mut app = bevy::app::App::new();
-/// hardcoded_things!(app, Test);
-/// ```
-#[macro_export]
-macro_rules! hardcoded_things {
-    ($app:expr, $($thing:ident),+) => {{
-        use hill_vacuum::MapThing;
-
-        let mut hardcoded_things = hill_vacuum::HardcodedThings::new();
-        $(hardcoded_things.push::<$thing>();)+
-        $app.insert_resource(hardcoded_things);
-    }}
-}
-
-//====================================================================
-
-/// Inserts the default properties that will be associated to all [`Brush`]es.
-/// # Example
-/// ```
-/// use hill_vacuum::{brush_properties, BrushProperties, Value};
-///
-/// let mut app = bevy::app::App::new();
-/// brush_properties!(app, [("Tag", 0u8), ("Destructible", false)]);
-/// ```
-#[macro_export]
-macro_rules! brush_properties {
-    ($app:expr, [$(($key:literal, $value:literal)),+]) => {
-        $app.insert_resource(hill_vacuum::BrushProperties::new([
-            $(($key, &$value as &dyn hill_vacuum::ToValue)),+
-        ]));
-    }
-}
-
-//====================================================================
-
-/// Inserts the default properties that will be associated to all [`ThingInstance`]s.
-/// # Example
-/// ```
-/// use hill_vacuum::{thing_properties, BrushProperties, Value};
-///
-/// let mut app = bevy::app::App::new();
-/// thing_properties!(app, [("Fire resistance", 1f32), ("Invisible", false)]);
-/// ```
-#[macro_export]
-macro_rules! thing_properties {
-    ($app:expr, [$(($key:literal, $value:literal)),+]) => {
-        $app.insert_resource(hill_vacuum::ThingProperties::new([
-            $(($key, &$value as &dyn hill_vacuum::ToValue)),+
-        ].into_iter()));
-    }
-}
-
-//=======================================================================//
 // UI
 //
 //=======================================================================//
@@ -171,6 +102,71 @@ pub(crate) mod ui_mod
 
     /// The name of the application.
     pub(crate) const NAME: &str = "HillVacuum";
+
+    //=======================================================================//
+    // MACROS
+    //
+    //=======================================================================//
+
+    /// Loads the desired [`Thing`]s as an available resource coded into the executable.
+    /// # Example
+    /// ```
+    /// use hill_vacuum::{hardcoded_things, MapThing, Thing};
+    ///
+    /// struct Test;
+    ///
+    /// impl MapThing for Test
+    /// {
+    ///     fn thing() -> Thing { Thing::new("test", 0, 32f32, 32f32, "test").unwrap() }
+    /// }
+    ///
+    /// let mut app = bevy::app::App::new();
+    /// hardcoded_things!(app, Test);
+    /// ```
+    #[macro_export]
+    macro_rules! hardcoded_things {
+        ($app:expr, $($thing:ident),+) => {{
+            use hill_vacuum::MapThing;
+
+            let mut hardcoded_things = hill_vacuum::HardcodedThings::new();
+            $(hardcoded_things.push::<$thing>();)+
+            $app.insert_resource(hardcoded_things);
+        }}
+    }
+
+    //====================================================================
+
+    /// Inserts the default properties that will be associated to all [`Brush`]es.
+    /// # Example
+    /// ```
+    /// let mut app = bevy::app::App::new();
+    /// hill_vacuum::brush_properties!(app, [("Tag", 0u8), ("Destructible", false)]);
+    /// ```
+    #[macro_export]
+    macro_rules! brush_properties {
+        ($app:expr, [$(($key:literal, $value:literal)),+]) => {
+            $app.insert_resource(hill_vacuum::BrushProperties::new([
+                $(($key, &$value as &dyn hill_vacuum::ToValue)),+
+            ]));
+        }
+    }
+
+    //====================================================================
+
+    /// Inserts the default properties that will be associated to all [`ThingInstance`]s.
+    /// # Example
+    /// ```
+    /// let mut app = bevy::app::App::new();
+    /// hill_vacuum::thing_properties!(app, [("Fire resistance", 1f32), ("Invisible", false)]);
+    /// ```
+    #[macro_export]
+    macro_rules! thing_properties {
+        ($app:expr, [$(($key:literal, $value:literal)),+]) => {
+            $app.insert_resource(hill_vacuum::ThingProperties::new([
+                $(($key, &$value as &dyn hill_vacuum::ToValue)),+
+            ].into_iter()));
+        }
+    }
 
     //=======================================================================//
     // ENUMS
